5aaf45d2fb1c4e1f04dcbeaf4b0c76b5bbeb6aac
Kai Schmid <kai.s.schmid@gmail.com>
2020-04-30 10:36:03 +0200

    Add files via upload

---
+++ R/load.R
@@ -0,0 +1,37 @@
+#' read.ab1 module
+#'
+#' This function reads in a given abi1-file
+#'
+#' @param filename location of a ab1 file.
+#' @param
+#'
+#' @return Objekt with all information taken from the given file.
+#'
+#' @export
+
+
+
+read.ab1 <- function(filename = ){
+
+  #read ab1file with sangerseqR
+  file <- sangerseqR::read.abif(filename = abi_file)
+
+  #extract B-number and genename
+  ids <- file@data$SMPL.1
+
+  #extract B-nummer
+
+  Bnummer <- strsplit(ids, "_")[[1]][1]
+
+  #extrat genename
+
+  genename <- strsplit(ids, "_")[[1]][2]
+  genename <- gsub('[[:lower:]]', '', genename)
+
+  #tranfer to fasta
+
+  filename <- paste0(Bnummer, "_", genename, ".fastq")
+  CrispRVariants::abifToFastq(seqname = filename, fname = abi_file, outfname = filename, cutoff = cutoff, min_seq_len = min_seq_len, offset = offset)
+
+
+}
